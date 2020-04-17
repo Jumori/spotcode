53315b2812b1849a239edfd5a2ad8de13e7ad58c
Jumori <julianamorikoshi@gmail.com>
2020-04-17 00:21:54 -0300

    rails api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,32 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   get 'home/index'
   root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  concern :favoritable do |options|
+    shallow do
+      post '/favorite', { to: 'favorites#create', on: :member }.merge(options)
+      delete '/favorite', { to: 'favorites#destroy', on: :member}.merge(options)
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :search, only: :index
+      resources :categories, only: [:index, :show]
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+        concerns :favoritable, favoritable_type: 'Album'
+      end
+      resources :favorites, only: :index
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end
+      resources :artists, only: [] do
+        concerns :favoritable, favoritable_type: 'Artist'
+      end
+    end
+  end
 end
