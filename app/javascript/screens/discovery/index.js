468484d14435b5462f01e80db2c0de2001170913
Jumori <julianamorikoshi@gmail.com>
2020-04-18 17:50:19 -0300

    navbar footer component

--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import NavbarFooter from './../../components/common/navbar_footer';
 
 export default function DiscoveryScreen() {
   return (
     <Fragment>
       <div>Discovery Screen</div>
+      <NavbarFooter />
     </Fragment>
   )
 }
--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import NavbarFooter from './../../components/common/navbar_footer';
 
 export default function FavoritesScreen() {
   return (
     <Fragment>
       <div>Favorites Screen</div>
+      <NavbarFooter />
     </Fragment>
   )
 }
