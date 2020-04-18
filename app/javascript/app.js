804c30889532888c37d5161491a4f0b98f53b602
Jumori <julianamorikoshi@gmail.com>
2020-04-17 22:52:33 -0300

    logo upper bar

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react';
 import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './app.scss';
+import Menu from './components/common/menu';
 
 function App() {
   return ( 
-    <Routes />
+    <Fragment>
+      <BrowserRouter>
+        <Menu />
+        <Routes />
+      </BrowserRouter>
+    </Fragment>
   )
 };
 
--- app/javascript/routes.js
+++ app/javascript/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomeScreen from './screens/home';
 import SearchScreen from './screens/search';
 import AlbumScreen from './screens/album';
@@ -8,14 +8,12 @@ import FavoritesScreen from './screens/favorites';
 
 export default function Routes() {
   return(
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={HomeScreen} />
-        <Route exact path='/search' component={SearchScreen} />
-        <Route exact path='/album/:id' component={AlbumScreen} />
-        <Route exact path='/discovery' component={DiscoveryScreen} />
-        <Route exact path='/favorites' component={FavoritesScreen} />
-      </Switch>  
-    </BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={HomeScreen} />
+      <Route exact path='/search' component={SearchScreen} />
+      <Route exact path='/album/:id' component={AlbumScreen} />
+      <Route exact path='/discovery' component={DiscoveryScreen} />
+      <Route exact path='/favorites' component={FavoritesScreen} />
+    </Switch>  
   )
 }
